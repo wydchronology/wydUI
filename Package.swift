55e56acf464f8a8cbe697fbd223b0493835eb865
Jason Ibrahim <jasonaibrahim@gmail.com>
2025-08-30 18:05:14 -0700

    chore: wip

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/siteline/swiftui-introspect", from: "1.4.0-beta.4"),
+        .package(url: "https://github.com/niklasamslgruber/EmojiKit", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -25,6 +26,7 @@ let package = Package(
             name: "wydUI",
             dependencies: [
                 .product(name: "SwiftUIIntrospect", package: "swiftui-introspect"),
+                .product(name: "EmojiKit", package: "EmojiKit")
             ]
         ),
     ],
